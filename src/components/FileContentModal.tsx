8c251868f8b73794d89c91e3ba371e4ae0bcb943
LavrovNick <lavrovmykola@gmail.com>
2024-07-27 07:51:47 +0300

    FileContentModal.tsx improvements

--- src/components/FileContentModal.tsx
+++ src/components/FileContentModal.tsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import CodeEditor from '@uiw/react-textarea-code-editor';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import Spinner from './Spinner';
 
 interface FileContentModalProps {
@@ -23,7 +21,6 @@ const FileContentModal: React.FC<FileContentModalProps> = ({
   isLoading,
   onSave,
 }) => {
-  const [editMode, setEditMode] = useState(false);
   const [editedContent, setEditedContent] = useState(content);
 
   useEffect(() => {
@@ -33,24 +30,48 @@ const FileContentModal: React.FC<FileContentModalProps> = ({
   if (!isOpen) return null;
 
   const handleSave = async () => {
-    await onSave(editedContent);
-    setEditMode(false);
+    onSave(editedContent);
   };
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4">
-      <div className="bg-white rounded-lg w-full max-w-3xl max-h-[90vh] flex flex-col">
-        <div className="flex justify-between items-center p-4 border-b">
+    <div
+      className="fixed inset-0 bg-black bg-opacity-50 flex-1 items-center justify-center z-10 p-64"
+      onClick={(e) => {
+        console.log("click on background");
+        e.preventDefault();
+        onClose();
+      }}
+    >
+      <div
+        className="flex flex-col bg-white rounded-lg"
+        onClick={(e) => {
+          console.log("click on forground");
+          e.preventDefault();
+        }}
+      >
+        <div className="flex justify-between items-center px-4 py-2">
           <h2 className="text-xl font-semibold">{fileName}</h2>
-          <div>
-            {!editMode && (
-              <button
-                onClick={() => setEditMode(true)}
-                className="text-blue-500 hover:text-blue-700 mr-4"
-              >
-                Edit
-              </button>
-            )}
+          <div className="space-x-4">
+            {editedContent !== content &&
+              (
+                <button
+                  onClick={() => setEditedContent(content)}
+                  className="text-gray-500 hover:text-gray-700 rounded px-2 py-1 border"
+                >
+                  Cancel
+                </button>
+              )
+            }
+            {editedContent !== content &&
+              (
+                <button
+                  onClick={async () => await handleSave()}
+                  className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600"
+                >
+                  Save
+                </button>
+              )
+            }
             <button
               onClick={onClose}
               className="text-gray-500 hover:text-gray-700"
@@ -59,52 +80,25 @@ const FileContentModal: React.FC<FileContentModalProps> = ({
             </button>
           </div>
         </div>
-        <div className="flex-grow overflow-auto p-4">
-          {isLoading ? (
-            <div className="flex justify-center items-center h-full">
-              <Spinner size="lg" color="blue" />
-            </div>
-          ) : editMode ? (
-            <div className="h-full">
+        <div>
+          {
+            isLoading ? (
+              <div className="flex justify-center items-center p-16">
+                <Spinner size="lg" color="blue" />
+              </div>
+            ) : (
               <CodeEditor
                 value={editedContent}
                 language={language}
-                placeholder=""
                 onChange={(evn) => setEditedContent(evn.target.value)}
-                padding={15}
                 style={{
-                  backgroundColor: "#000",
                   fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
                 }}
                 data-color-mode={'dark'}
               />
-            </div>
-          ) : (
-            <SyntaxHighlighter
-              language={language}
-              style={vscDarkPlus}
-              customStyle={{ margin: 0 }}
-            >
-              {editedContent}
-            </SyntaxHighlighter>
-          )}
+            )
+          }
         </div>
-        {editMode && (
-          <div className="flex justify-end p-4 border-t">
-            <button
-              onClick={() => setEditMode(false)}
-              className="text-gray-500 hover:text-gray-700 mr-4"
-            >
-              Cancel
-            </button>
-            <button
-              onClick={async () => await handleSave()}
-              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-            >
-              Save
-            </button>
-          </div>
-        )}
       </div>
     </div>
   );
