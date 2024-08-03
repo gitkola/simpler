f7ea36d2a728ca696d49b0fd8c2b019481e3668c
LavrovNick <lavrovmykola@gmail.com>
2024-08-03 04:40:04 +0300

    clean code

--- src/components/FilesCompareView.tsx
+++ src/components/FilesCompareView.tsx
@@ -10,25 +10,23 @@ export default function FilesCompareView() {
 
   return (
     <div className="flex flex-col h-screen border-r border-0.5 min-w-[800px] overflow-x-scroll">
-      <div className="flex p-2 space-x-2 items-center justify-start border-b border-0.5">
-        <Diff className="w-8 h-8" />
-        <h2 className="text-lg font-semibold">Compare View</h2>
+      <div className="flex p-2 space-x-2 items-center justify-between border-b border-0.5">
+        <div className="flex">
+          <Diff className="w-8 h-8" />
+          <h2 className="text-lg font-semibold">Compare View</h2>
+        </div>
         <SquareButton icon="refresh" onClick={() => { }} className="w-8 h-8 " />
       </div>
-      {
-        isLoadingCurrentProjectOpenedFiles && <ProcessIndicator />
-      }
-      {
-        currentProjectOpenedFilesError && <div>{currentProjectOpenedFilesError}</div>
-      }
+      {isLoadingCurrentProjectOpenedFiles && <ProcessIndicator />}
+      {currentProjectOpenedFilesError && <div>{currentProjectOpenedFilesError}</div>}
       <TabsView>
-        {currentProjectOpenedFiles.map((file) => (
-          <TabContent path={file.path} key={file.path} isActive={!!file.isActive}>
-            {
-              !!file.isActive && <CompareViewer path={file.path} />
-            }
-          </TabContent>
-        ))}
+        {
+          currentProjectOpenedFiles.map((file) => (
+            <TabContent path={file.path} key={file.path} isActive={!!file.isActive}>
+              {!!file.isActive && <CompareViewer path={file.path} />}
+            </TabContent>
+          ))
+        }
       </TabsView>
     </div>
   );
