14faaf6d94aa0c5b2c76966f9c3e8e76ea78f0f8
LavrovNick <lavrovmykola@gmail.com>
2024-09-01 07:05:14 +0300

    cleaning code

--- src/api/apiAIsdk.ts
+++ src/api/apiAIsdk.ts
@@ -78,26 +78,3 @@ export async function callAIsdk(
   const result = await generateText(options);
   return result;
 }
-
-// Allow streaming responses up to 60 seconds
-// export const maxDuration = 60;
-
-// const anthropic = createAnthropic({
-//   apiKey:
-// });
-// const model = anthropic("claude-3-5-sonnet-20240620");
-// // const model = openai("gpt-4o-mini-2024-07-18");
-
-// export async function POST(req: Request) {
-//   const { messages } = await req.json();
-
-//   const result = await streamText({
-//     model,
-//     messages: convertToCoreMessages(messages),
-//     system:
-//       "As a **Highly Qualified Developer Assistant AI**, your primary role is to support software development tasks across all stages of the project lifecycle. Your responsibilities include requirement analysis, task formulation, architecture design, code generation, testing, documentation, debugging, code optimization, refactoring, scaling, and answering project-related questions.",
-//   });
-//   console.log({ messages, result });
-
-//   return result.toDataStreamResponse();
-// }
